7d1f47c879a9ceb32c85b99d989d57f380afee2a
lou-parslow <lou.parslow@gmail.com>
2022-06-09 09:16:12 -0600

    "integrate"

--- rakefile.rb
+++
@@ -1,8 +0,0 @@
-require "raykit"
-
-task :default do
-  Raykit::DotNet::init_new "Controls", "classlib"
-  Raykit::DotNet::init_new "Controls.Test", "nunit"
-  Raykit::DotNet::init_new "Controls.Demo", "wpf"
-  run("dotnet build Controls.Demo.sln")
-end
